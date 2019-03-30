f0eea35419b25d1ed3137aea2217728c4a5f6a40
aslihanozfidan <aslihanozfidan@gmail.com>
2019-03-30 22:01:42 +0300

    change image api with lorem picsum

--- src/components/RandomBackground/index.js
+++ src/components/RandomBackground/index.js
@@ -1,43 +1,18 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom'
-import PropTypes from "prop-types";
 import "./style.css";
 
 class RandomBackground extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      image: ""
-    };
-
-    this.baseApiUrl = "http://35.241.204.67/photos/"
   }
 
-  getRandomNumber = () => {
-    return Math.floor(Math.random() * 8);
-  };
-
-  setRandomFullSizeBackground = number => {
-    return `${this.baseApiUrl}${number}_mini.jpg`
-  };
-
   render() {
-    const randomNumber = this.getRandomNumber();
-
     return (
       <div
-        style={{backgroundImage: `url(${this.setRandomFullSizeBackground(randomNumber)})`}}
+        style={{backgroundImage: `url(https://picsum.photos/200/?random)`}}
         className="random-image" />
     );
   }
 }
-
-RandomBackground.defaultProps = {
-  image: ""
-};
-
-RandomBackground.propTypes = {
-  image: PropTypes.string
-};
 
 export default RandomBackground;
