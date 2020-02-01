e70f5b6eb6f304d653fb9202fc80659acfd0ed48
Aslihan <aslihanozfidan@gmail.com>
2020-02-01 19:33:26 +0300

    set chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,109 +1,115 @@
-var path = require("path");
-const TerserPlugin = require('terser-webpack-plugin');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+var path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-const SRC_DIR = path.join(__dirname, "src");
-const DIST_DIR = path.join(__dirname, "dist");
+const SRC_DIR = path.join(__dirname, 'src')
+const DIST_DIR = path.join(__dirname, 'dist')
 
 module.exports = {
-	mode: "production",
-	entry: "./src/index",
-	output: {
-		path: DIST_DIR,
-		filename: "bundle.js"
-	},
-	resolve: {
-		extensions: [
-			".js",
-			".css"
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /(\.css$)/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					"css-loader",
-				]
-			},
-			{ test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: "url-loader?limit=1000000" },
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: "html-loader",
-						options: { minimize: true }
-					}
-				]
-			},
-			{
-				test: /\.js$/,
-				include: SRC_DIR,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				query: {
-					presets: [
-						"@babel/react",
-						"@babel/preset-env"
-					],
-					plugins: [
-						"@babel/proposal-class-properties"
-					]
-				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "src/index.html"
-		}),
-		new MiniCssExtractPlugin({
-			path: DIST_DIR,
-			filename: "main.css"
-		}),
-		new CompressionPlugin({
-			algorithm: "gzip",
-			deleteOriginalAssets: true,
-			test: /\.js$|\.css$|\.html$/,
-			threshold: 10240,
-			minRatio: 0
-		}),
-		new OptimizeCssAssetsPlugin({
-			canPrint: true,
-			minimize: true
+  mode: 'production',
+  entry: './src/index',
+  output: {
+    path: DIST_DIR,
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].[id].bundle.js'
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.css'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.css$)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader'
+        ]
+       
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=1000000' },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            '@babel/react',
+            '@babel/preset-env'
+          ],
+          plugins: [
+            '@babel/proposal-class-properties'
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      path: DIST_DIR,
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      deleteOriginalAssets: true,
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0
     })
-		//new BundleAnalyzerPlugin()
-	],
-	optimization: {
-		minimizer: [
-			new TerserPlugin({
-				cache: true,
-			}),
-			new UglifyJsPlugin({
-				cache: true,
-				parallel: true,
-				uglifyOptions: {
-					warnings: false,
-					parse: {},
-					compress: {},
-					mangle: true, // Note `mangle.properties` is `false` by default.
-					output: {
-						comments: false
-					},
-					toplevel: false,
-					nameCache: null,
-					ie8: true,
-					keep_fnames: false
-				}
-			})
-		]
-	}
-};
+    //new BundleAnalyzerPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    minimizer: [
+      new TerserPlugin({
+        cache: true
+      }),
+      new OptimizeCssAssetsPlugin({
+        canPrint: true,
+        minimize: true
+      }),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          output: {
+            comments: false
+          },
+          toplevel: false,
+          nameCache: null,
+          ie8: true,
+          keep_fnames: false
+        }
+      })
+    ]
+  }
+}
