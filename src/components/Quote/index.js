cf5769c6cf3ef1d9cf8e466082bd6692228f6e05
Aslihan <aslihanozfidan@gmail.com>
2020-02-02 20:00:46 +0300

    add next quote tour

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 import Loading from '../Loading'
 import './style.css'
 
-function Quote({ loading, quote, author, nextClick }) {
+function Quote({ loading, quote, author, isOpenTour, nextClick }) {
   return (
     <div>
       <div className='quote-text-container'>
         <Loading classNames={`loading-container ${loading ? 'show' : 'hidden'}`} />
         <p className={`quote-text ${loading ? 'hidden' : 'show'}`}>{quote}</p>
-        <a className='btn-next' onClick={() => nextClick()} />
+        <a className={`btn-next`} onClick={() => nextClick()} />
+
+        <div className={`tour-container ${isOpenTour ? 'show' : 'hidden'}`}>
+          <span className='tour-pulsing-circle' />
+          <div className='text'>Click here to see a new quote</div>
+        </div>
       </div>
       <p className={`quote-author ${loading ? 'hidden' : 'show'}`}>{author}</p>
     </div>
--- src/containers/Quote/index.js
+++ src/containers/Quote/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import Quote from '../../components/Quote'
 import QuoteService from './../../services/QuoteService'
+import Cookies from 'js-cookie'
 import './style.css'
 
 class QuoteContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quote: '',
-      author: '',
-      loading: false
+      quote: null,
+      author: null,
+      loading: false,
+      isOpenTour: false
     }
 
     this.quoteService = new QuoteService()
@@ -21,6 +23,13 @@ class QuoteContainer extends Component {
     if (quoteData.quote.length > 120) {
       this.getRandomQuote()
     }
+
+    if (!this.hasTourCookie()) {
+      this.setTourCookie(true)
+    } else {
+      this.setState({ isOpenTour: false })
+    }
+
     this.setState({
       quote: quoteData.quote,
       author: quoteData.author,
@@ -28,18 +37,33 @@ class QuoteContainer extends Component {
     })
   }
 
+  hasTourCookie = () => {
+    if (Cookies.get('show_tour') == 'true') {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  setTourCookie = (boolValue) => {
+    Cookies.set('show_tour', boolValue)
+    this.setState({ isOpenTour: boolValue })
+  }
+
   componentDidMount = () => {
     this.getRandomQuote()
   }
 
   render() {
+    const { author, quote, loading, isOpenTour } = this.state
     return (
       <div className='flex'>
         <div className='quote-container'>
           <Quote
-            author={this.state.author}
-            quote={this.state.quote}
-            loading={this.state.loading}
+            author={author}
+            quote={quote}
+            loading={loading}
+            isOpenTour={isOpenTour}
             nextClick={() => this.getRandomQuote()}
           />
         </div>
